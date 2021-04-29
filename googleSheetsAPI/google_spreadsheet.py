3011b8148e44cc35730f516dab9a2a2972a93c5b
valen22br <valensuela@gmail.com>
2021-04-28 20:59:12 -0400

    Update clientToSpreadsheetMap with new data

--- googleSheetsAPI/google_spreadsheet.py
+++ googleSheetsAPI/google_spreadsheet.py
@@ -72,6 +72,7 @@ class GoogleSpreadsheet:
             'sist':(['148500071','SORRIDENTS_ITAIM_SILVATELES_TIBURCIO'],[8,120,8,8,12,15]),
             'ssc':(['786524798','SORRIDENTS_CURUCA'],[8,120,8,8,12,15]),
             'sgva':(['921845397','SORRIDENTS_GETULIO_VARGAS'],[8,120,8,8,12,15]),
+            'stp':(['1619069009','SORRIDENTS_TATUAPE'],[8,120,8,8,12,15]),
             'test':(['1413480013','test'],[8,120,8,8,12,15]),
          }
         self.spreadsheetID = '1n7fNL39lksm8Vua7jBW8CaTmqGi7Y-_s6ITB0JPxGOk'
